8a3decfb2b2f8fa38c9ffeefe0897a53fc3397ca
BYteSL <BYteSL@users.noreply.github.com>
2020-01-22 23:54:52 +0000

    Using the if and Else

---
+++ C files/Exercises/prog2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/*
+Autor:      Yuri Lemos
+Drescrição: Programa que le 2 numero e indica o maior,
+            caso sejam iguais, escreve que sao iguais.
+Data:       06/10/2019
+*/
+
+void main(){
+    int numero1,numero2,resto;
+    printf("Insira o primeiro numero: ");
+    scanf("%d",&numero1);    //INPUT DO NUMERO 1 PELO UTILIZADOR
+    printf("Insira o segundo numero: ");
+    scanf("%d",&numero2);    //INPUT DO NUMERO 2 PELO UTILIZADOR
+
+    if(numero1==numero2){         //VERIFICA SE O RESTO DA DIVISAO É ZERO
+        printf("Os numeros sao iguais!");
+        fflush(stdin);
+        getchar();
+    }else if(numero1>numero2){
+        printf("O nummero %d e o maior",numero1);
+        fflush(stdin);
+        getchar();
+    }else{
+        printf("O nummero %d e o maior",numero2);
+        fflush(stdin);
+        getchar();
+    }
+}
